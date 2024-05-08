35c0b5b10be7d4ab47ce3d593499aa6cb0e6c3b3
jokyo3 <150494320+jokyo3@users.noreply.github.com>
2024-05-08 16:46:10 +0800

    Update album-management.js

--- album-management.js
+++ album-management.js
@@ -68,7 +68,7 @@ function uploadImageAndGetFullUrl(uploadEndpoint, hostUrl, file) {
 
 // 定义host URL和上传端点
 const hostUrl = 'https://imghost.wook.eu.org';
-//const hostUrl = 'https://telegra.ph';
+//const hostUrl = 'https://' + window.location.hostname;
 const uploadEndpoint = '/upload';
 
 // 在文档加载完成后添加事件监听器
